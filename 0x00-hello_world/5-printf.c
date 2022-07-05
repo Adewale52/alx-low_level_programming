d45850b3dde4c9dfa20345a60c6b6bb8e248b455
adewale52 <adeoyeadewale52@gmail.com>
2022-07-04 18:10:10 -0700

    .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
-  *main - print the phrase with proper grammar
-  *return - returns the value 0
-  */
-
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
+
