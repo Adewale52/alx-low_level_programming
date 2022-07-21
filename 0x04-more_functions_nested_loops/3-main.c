c39004a7930a648dc2dde94a90607f255e6a30c0
adewale52 <adeoyeadewale52@gmail.com>
2022-07-21 15:03:52 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * main - checks the code
+  * Return: Always zero
+  */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _isupper(int c);
 int _isdigit(int c);
 void print_most_numbers(void);
 int mul(int a, int b);
-
+void print_numbers(void);
 
 
 
