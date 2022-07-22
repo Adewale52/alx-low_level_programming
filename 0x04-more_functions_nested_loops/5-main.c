dd18576e0ed9170db478905171b87c77c7fdcc96
adewale52 <adeoyeadewale52@gmail.com>
2022-07-21 17:43:04 -0700

    prints 10 times the numbers, from 0 to 14, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * main - checs the code
+  * Return: Always zero
+  */
+int main(void)
+{
+	more_numbers();
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int _isdigit(int c);
 void print_most_numbers(void);
 int mul(int a, int b);
 void print_numbers(void);
-
+void more_numbers(void);
 
 
 #endif
