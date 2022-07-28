512953aff55cedcd54ca72ea1d51faf4cb957af7
adewale52 <adeoyeadewale52@gmail.com>
2022-07-27 19:01:10 -0700

    Print an integer

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - checks the code
+  * Return: Always zero
+  */
+int main(void)
+{
+	print_number(200);
+	_putchar('\n');
+	print_number(-200);
+	_putchar('\n');
+
+	return (0);
+}
