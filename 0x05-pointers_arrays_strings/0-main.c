078fcefdfed976e0bef462576776df0662648dba
adewale52 <adeoyeadewale52@gmail.com>
2022-09-06 04:10:14 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - checks the code
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
